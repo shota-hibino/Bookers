702f7636a184b4b2a7e6280dd42acea72040f4d4
shota-hibino <yamoushou@gmail.com>
2021-04-12 03:06:13 +0000

    [Update]update update action fix & destory action create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,9 +22,15 @@ class BooksController < ApplicationController
   end
 
   def update
-    @book = Book.find(params[:id])
+    book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to books_path(book.id)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
